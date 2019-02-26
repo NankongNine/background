50c2f93a4939f6448179b15215ccc18976a0fd6f
NankongNine <septem919@hotmail.com>
2019-02-26 23:29:44 +0800

    安全认证过滤

--- src/main/java/com/threewater/template/background/config/WebSecurityConfig.java
+++ src/main/java/com/threewater/template/background/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -42,7 +43,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .passwordParameter("password")
                 .and()
                 .logout().permitAll();
-
+//        http.authorizeRequests()
+//                //不需要身份认证
+//                .antMatchers("/", "/home","/toLogin","/**/customer/**").permitAll()
+//                .antMatchers("/js/**", "/css/**", "/images/**", "/fronts/**", "/doc/**", "/toLogin").permitAll()
+//                .antMatchers("/user/**").hasAnyRole("USER")
+//                //.hasIpAddress()//读取配置权限配置
+//                .antMatchers("/**").access("hasRole('ADMIN')")
+//                .anyRequest().authenticated()
+//                //自定义登录界面
+//                .and().formLogin().loginPage("/toLogin").loginProcessingUrl("/login").failureUrl("/toLogin?error").permitAll()
+//                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .and().exceptionHandling().accessDeniedPage("/toLogin?deny")
+//                .and().httpBasic()
+//                .and().sessionManagement().invalidSessionUrl("/toLogin")
+//                .and().csrf().disable();
         // 关闭CSRF跨域
         http.csrf().disable();
     }
